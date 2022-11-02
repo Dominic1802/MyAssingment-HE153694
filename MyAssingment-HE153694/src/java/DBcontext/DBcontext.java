1496da58ff87d55db50d8cd57d75e518c39cb0bd
Dominic1802 <115022066+Dominic1802@users.noreply.github.com>
2022-11-02 23:07:58 +0700

    Code

--- MyAssingment-HE153694/src/java/DBcontext/DBcontext.java
+++ MyAssingment-HE153694/src/java/DBcontext/DBcontext.java
@@ -1,14 +1,33 @@
-package DBcontext;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package DBcontext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author 84868
+ * @author Ngo Tung Son
  */
-public class DBcontext {
-    
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String username = "sa";
+            String password = "123456";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
